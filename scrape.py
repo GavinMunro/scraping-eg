5c94ef8022579f5a1211af3779fec9b7e9faa68e
gavin <ob1.data@gmail.com>
2020-12-21 16:57:21 +1000

    Twitter blocks requests.get so use webdriver.Chrome

--- scrape.py
+++ scrape.py
@@ -2,14 +2,28 @@
 Gavin A.I. Munro
 """
 
+import os
+import time
+from pathlib import Path
+
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from selenium import webdriver
+from dotenv import load_dotenv
 
 
-def grab_html(url="https://twitter.com"):
-    page = urlopen(url)
-    html = page.read().decode("utf-8")
-    soup = BeautifulSoup(html, "html.parse")
+env_path = Path('.') / Path('.env')
+load_dotenv(dotenv_path=str(env_path))
+# The path to the local chromedriver executable should now be CHROMEDRIVER_PATH
+chromedriver_path = os.getenv("CHROMEDRIVER_PATH")
+
+
+def grab_html(url="https://twitter.com/BorisJohnson"):
+    browser = webdriver.Chrome(executable_path=chromedriver_path)
+    browser.get(url)
+    time.sleep(5)
+    html = browser.page_source
+    soup = BeautifulSoup(html, "html5lib")
+    browser.close()
     return soup
 
 
